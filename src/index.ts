8b9fe23e0bd080d56529e2dbffb41a8ac5f5a137
Meeep <139204086+Meeepu@users.noreply.github.com>
2023-08-31 10:06:21 +0800

    Added database connection

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import mongoose from 'mongoose';
 
 import errorHandler from 'middlewares/errorHandler';
 import authenticate from 'middlewares/authenticate';
+import envs from 'utilities/envs';
+
 const app = express();
 
 app.use(cookieParser());
@@ -13,3 +15,9 @@ app.use(express.json());
 app.use(helmet());
 app.use(authenticate);
 app.use(errorHandler);
+
+mongoose.connect(envs.MONGO_URI).then(() => {
+    console.log('Connected to database');
+    app.listen(envs.PORT, () => console.log(`Server is running on port ${envs.PORT}`));
+});
+
