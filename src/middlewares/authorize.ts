562b441b6ccbf2d5e0a1c59879cc5ed6acef0cf8
Meeep <139204086+Meeepu@users.noreply.github.com>
2023-08-31 21:26:47 +0800

    remove unnecessary check for store owner

--- src/middlewares/authorize.ts
+++ src/middlewares/authorize.ts
@@ -1,25 +1,9 @@
 import { RequestHandler } from 'express';
 import { UserRoles } from '../api/user/user.model';
 import { Forbidden } from '../utilities/errors';
-import StoreModel, { StoreDocument } from '../api/store/store.model';
 
-export const admin: RequestHandler = (req, _res, next) => {
+export const onlyAdmin: RequestHandler = (req, _res, next) => {
     if (req.user?.role === UserRoles.ADMIN) return next();
 
     next(new Forbidden('This action requires admin privileges'));
-};
-
-export const owner: RequestHandler = async (req, _res, next) => {
-    const { storeId } = req.body || req.query;
-    try {
-        const store: StoreDocument | null = await StoreModel.findOne({ storeId, owner: req.user?._id })
-            .populate('owner')
-            .exec();
-        if (store === null) throw new Forbidden();
-
-        req.store = store;
-    } catch {
-    } finally {
-        next();
-    }
 };
