356ea572040e2e389cb4b871b42c099a05d64334
Meeep <139204086+Meeepu@users.noreply.github.com>
2023-09-01 12:55:39 +0800

    Added condition to create admin account on server start

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,38 @@
+import UserModel, { UserDocument, UserRoles } from './api/user/user.model';
 import app from './app';
 import database from './database';
 import envs from './utilities/envs';
 
-database.then(() => {
-    console.log('Connected to database');
-    app.listen(envs.PORT, () => console.log(`Server is running on port ${envs.PORT}`));
-});
+// Connect to the database
+database
+    .then(() => {
+        console.log('Connected to database');
+
+        // Find the admin user in the database
+        return UserModel.findOne({ 'credentials.email': envs.ADMIN_EMAIL });
+    })
+    .then((admin: UserDocument | null) => {
+        // If the admin user does not exist, create it
+        if (admin === null) {
+            return UserModel.create({
+                name: {
+                    first: 'Admin',
+                    last: 'User'
+                },
+                credentials: {
+                    email: envs.ADMIN_EMAIL,
+                    password: envs.ADMIN_PASS
+                },
+                role: UserRoles.ADMIN
+            });
+        }
+
+        // Return the existing admin user
+        return admin;
+    })
+    .then(() => {
+        // Start the server and listen on the specified port
+        app.listen(envs.PORT, () => console.log(`Server is running on port ${envs.PORT}`));
+    });
 
 export default app;
