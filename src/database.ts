9da0d3033b99a0bb324ca9d5f1fbec5b9c23ed43
Meeep <139204086+Meeepu@users.noreply.github.com>
2023-09-02 14:34:20 +0800

    refactor: change how server file is defined

--- src/database.ts
+++
@@ -1,4 +0,0 @@
-import envs from './utilities/envs';
-import mongoose from 'mongoose';
-
-export default mongoose.connect(envs.MONGO_URI);
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,69 @@
-import UserModel, { UserDocument, UserRoles } from './api/user/user.model';
-import app from './app';
-import database from './database';
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import envs from './utilities/envs';
+import express from 'express';
+import helmet from 'helmet';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yamljs';
+
+// Import custom middlewares and routes
+import errorHandler from './middlewares/errorHandler';
+import authenticate from './middlewares/authenticate';
+import authRoute from './api/auth/auth.route';
+import storeRoute from './api/store/store.route';
+import userRoute from './api/user/user.route';
+
+// Import custom utilities and models
+import { NotFound } from './utilities/errors';
+import UserModel, { UserDocument, UserRoles } from './api/user/user.model';
+
+// Create Express application
+const app = express();
+
+// Use middlewares
+app.use(cors({ credentials: true }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('dev'));
+
+// Load Swagger documentation
 const swaggerDoc = yaml.load('./swagger.yaml');
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
+// Allow cross-origin requests
+app.use((_req, res, next) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+// Mount routes
+app.use('/auth', authRoute);
+app.use(authenticate);
+app.use('/stores', storeRoute);
+app.use('/users', userRoute);
+
+// Handle not found routes
+app.use((_req, _res, next) => next(new NotFound()));
+
+// Handle errors
+app.use(errorHandler);
+
 // Connect to the database
-database
+mongoose
+    .connect(envs.MONGO_URI)
     .then(() => {
         console.log('Connected to database');
 
-        // Find the admin user in the database
-        return UserModel.findOne({ 'credentials.email': envs.ADMIN_EMAIL });
+        // Find or create an admin user
+        return UserModel.findOne({ role: UserRoles.ADMIN });
     })
     .then((admin: UserDocument | null) => {
-        // If the admin user does not exist, create it
         if (admin === null) {
+            // Create a new admin user if not found
             return UserModel.create({
                 name: {
                     first: 'Admin',
@@ -29,12 +77,12 @@ database
             });
         }
 
-        // Return the existing admin user
         return admin;
     })
     .then(() => {
-        // Start the server and listen on the specified port
+        // Start the server
         app.listen(envs.PORT, () => console.log(`Server is running on port ${envs.PORT}`));
-    });
+    })
+    .catch((error) => console.log('Error connecting to database: ', error));
 
 export default app;
