3744c67770e9b3b2d39a10f4b8badcb2bfb783b7
Meeep <139204086+Meeepu@users.noreply.github.com>
2023-09-03 15:34:34 +0800

    test: create admin account per test

--- test/setup.test.ts
+++ test/setup.test.ts
@@ -8,6 +8,27 @@ beforeEach(async () => {
     await UserModel.deleteMany({});
     await StoreModel.deleteMany({});
 
+    // Find a document in the UserModel collection that has the role "ADMIN" and update it
+    // If no document is found, create a new document with the specified data
+    await UserModel.findOneAndUpdate(
+        { role: UserRoles.ADMIN },
+        {
+            // Update the name field of the document
+            name: {
+                first: 'Admin',
+                last: 'User'
+            },
+            // Update the credentials field of the document
+            credentials: {
+                email: envs.ADMIN_EMAIL,
+                password: envs.ADMIN_PASS
+            },
+            // Update the role field of the document
+            role: UserRoles.ADMIN
+        },
+        // Specify the "upsert" option to create a new document if it doesn't exist
+        { upsert: true }
+    );
 });
 
 afterEach(async () => {
